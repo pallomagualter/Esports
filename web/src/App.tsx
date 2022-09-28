e3a4c903f3c02c231aa938d3c76589862aa996eb
pallomagualter <pallomagualter@gmail.com>
2022-09-28 16:31:08 -0300

    feat: added to the project the web frontend with react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,22 @@
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello NLW</h1>
+      <Button title="Send 1" />
+      <Button title="Excluir" />
+    </>
   )
 }
 
